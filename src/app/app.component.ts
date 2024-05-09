540086f0ba1f011a9194f538e55df10b081ab25e
Mohammad Sharfuddin Khan <79760546+sharfuddin1984@users.noreply.github.com>
2024-05-09 10:51:04 +0530

    add kishopr task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'k-app';
+  title = 'k-app'; 
+  Claimant: any;
+  claimTypeACode: any;
+  injuryDamageCode: any;
+  linePolicy: any;
+  adjuster: any;
+  claimDetails: any;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatSelectModule} from '@angular/material/select';
+import { TableModule } from 'primeng/table';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSelectModule,
+    TableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
